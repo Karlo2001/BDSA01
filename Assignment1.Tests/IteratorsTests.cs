1d32b6b987334c1582104c5a393703752f145cb3
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-10 13:48:39 +0200

    Implemented flatten with test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,21 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+       
+        [Fact]
+        public void Test1()
+        {
+            IEnumerable<IEnumerable<string>> inputList = new List<List<string>> {new List<string> {"a","b","c"}, new List<string> {"d", "e", "f"}};
+            IEnumerable<string> expectedList = new List<string> {"a", "b", "c", "d", "e", "f"};
+
+            IEnumerable<string> resultList = Iterators.Flatten<string>(inputList);
+
+            Assert.Equal(expectedList, resultList);
+        }
     }
 }
