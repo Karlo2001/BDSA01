e84033e6c7b39cd10e4a284680554f1eb316a577
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-13 15:29:12 +0200

    Implemented splitline and resolutions with test

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,54 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void SplitLine_Returns_All_Words()
+        {
+            IEnumerable<string> input = new List<string> {"All these words should be captured individually", "Also these ones"};
+            IEnumerable<string> expected = new List<string> {"All", "these", "words", "should", "be", "captured", "individually", "Also", "these", "ones"};
+
+            IEnumerable<string> result = RegExpr.SplitLine(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void SplitLine_Returns_Words_With_Digits()
+        {
+            IEnumerable<string> input = new List<string> {"1aksa 25asd2 akb24"};
+            IEnumerable<string> expected = new List<string> {"1aksa", "25asd2", "akb24"};
+
+            IEnumerable<string> result = RegExpr.SplitLine(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Resolutions_Returns_Tuple()
+        {
+            var input = "1920x1080, 1024x768, 1280x960";
+            IEnumerable<(int, int)> expected = new List<(int, int)> {(1920, 1080), (1024,768), (1280, 960)};
+            
+            IEnumerable<(int, int)> result = RegExpr.Resolution(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void InnerText_Returns_All_Inner_Text()
+        {
+            var inputHTML = "<div> <p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p> </div>";
+            var inputTag = "p";
+            IEnumerable<string> expected = new List<string> {"The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to."};
+
+            IEnumerable<string> result = RegExpr.InnerText(inputHTML, inputTag);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,17 +8,43 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach (var word in lines)
+            {
+                var pattern = @"[a-zA-z0-9]+";
+                Regex rgx = new Regex(pattern);
+
+                foreach (Match match in rgx.Matches(word))
+                {
+                    yield return match.Value;
+                }
+            }
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
         {
-            throw new NotImplementedException();
+            var pattern = @"(?<width>\d{3,5})x(?<height>\d{3,5})";
+            Regex rgx = new Regex(pattern);
+
+            foreach (Match match in rgx.Matches(resolutions))
+            {
+
+                int width = Int32.Parse(match.Groups[1].Value);
+                int height = Int32.Parse(match.Groups[2].Value);
+
+                yield return (width, height);
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
         {
-            throw new NotImplementedException();
+            //Doesnt work yet!!
+            var pattern = @"(<(" + tag + ")>)+((?<Inner_Text>[a-zA-Z]+)|(?:<.+>)*?)+(</\\2>)";
+            Regex rgx = new Regex(pattern);
+            GroupCollection groups = rgx.Match(html).Groups;
+            foreach (Match match in rgx.Matches(html))
+            {
+                yield return groups["Inner_Text"].Value;
+            }
         }
     }
 }
