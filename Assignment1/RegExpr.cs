96e27f9518404cac52c6b1f238f694e8797f85ab
Karlo2001 <43517053+Karlo2001@users.noreply.github.com>
2021-09-13 17:59:48 +0200

    Regex (#1)
    * Implemented splitline and resolutions with test
    
    * Implemented InnerText

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,17 +8,43 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach (var word in lines)
+            {
+                var pattern = @"[a-zA-z0-9]+";
+                Regex rgx = new Regex(pattern);
+
+                foreach (Match match in rgx.Matches(word))
+                {
+                    yield return match.Value;
+                }
+            }
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
         {
-            throw new NotImplementedException();
+            var pattern = @"(?<width>\d{3,5})x(?<height>\d{3,5})";
+            Regex rgx = new Regex(pattern);
+
+            foreach (Match match in rgx.Matches(resolutions))
+            {
+
+                int width = Int32.Parse(match.Groups[1].Value);
+                int height = Int32.Parse(match.Groups[2].Value);
+
+                yield return (width, height);
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
         {
-            throw new NotImplementedException();
+            var pattern = @"<(" + tag + ")>(.+)<\\/\\1>";
+            var replacePattern = @"(<[^>]+>)+";
+            Regex rgx = new Regex(pattern);
+            foreach (Match match in rgx.Matches(html))
+            {
+                Regex reg = new Regex(replacePattern);
+                yield return reg.Replace(match.Groups[2].Value, @"");
+            }
         }
     }
 }
